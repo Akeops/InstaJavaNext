aeb9f7105226fe1d761fc3d90d1c124e302717ac
Andoni Lalanne-Berdouticq <39518930+Akeops@users.noreply.github.com>
2024-09-21 10:36:18 +0200

    first commit

---
+++ src/main/java/com/example/instaJavaNext/InstaJavaNextApplication.java
@@ -0,0 +1,13 @@
+package com.example.instaJavaNext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstaJavaNextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstaJavaNextApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/instaJavaNext/InstaJavaNextApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.instaJavaNext;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstaJavaNextApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
